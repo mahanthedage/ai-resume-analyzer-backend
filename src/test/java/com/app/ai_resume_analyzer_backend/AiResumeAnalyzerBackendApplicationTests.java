ec58bff63e4e9fda4fb1b9a10e96abf76de05c93
Mahantesh <mahanteshhedage75@gmail.com>
2025-09-13 20:48:43 +0530

    Initial Spring Boot app added

---
+++ src/test/java/com/app/ai_resume_analyzer_backend/AiResumeAnalyzerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ai_resume_analyzer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiResumeAnalyzerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
